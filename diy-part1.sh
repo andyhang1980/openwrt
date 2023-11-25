3efadcbfab3b026dd280d5d05db94fda2fc6718e
andy zhang <63940134+andyhang1980@users.noreply.github.com>
2023-11-25 09:42:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,6 @@
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+sed -i '$a src-git liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
 
